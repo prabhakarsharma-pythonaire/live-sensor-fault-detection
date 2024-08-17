5ad598dc825f2c67efb58f9293c2fe99aa6e84b7
Prabhakar <prabhakarkumar313@gmail.com>
2024-08-18 01:46:08 +0530

    logging and exception completed

---
+++ main.py
@@ -0,0 +1,21 @@
+from Sensor.exception import SensorException
+import os,sys
+
+from Sensor.logger import logging
+
+
+
+def test_exception():
+    try:
+        logging.info("intentionally causing error to check if it's working")
+        b = 1/0
+
+    except Exception as e:
+        raise SensorException(e,sys)
+
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
