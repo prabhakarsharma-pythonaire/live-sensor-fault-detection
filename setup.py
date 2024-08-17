85eb723d9791123400408ca1dc901e69adb2a0ce
Prabhakar <prabhakarkumar313@gmail.com>
2024-08-17 19:48:26 +0530

    basic things added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def get_requirements()->list[str]:
+
+    requirements_list = list[str] = []
+
+    return requirements_list
+
+
+
+
+setup (
+
+    name = "Sensor",
+    version = "0.0.1",
+    author = "Prabhakar",
+    author_email = "Prabhakarkumar313@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
+
+
